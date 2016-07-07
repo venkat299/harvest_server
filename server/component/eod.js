cc76da9c3ee10270904c7790c6f92ec79dab9fab
Venkatesh Duraiarasan <dev_venkat@outlook.com>
2016-07-08 04:54:12 +0530

    fixed eod retrieval in strategy page

--- server/component/eod.js
+++ server/component/eod.js
@@ -2,31 +2,9 @@
 // private lib
 // =======================
 var routes = require('express').Router();
-// ========== logout 
-// routes.get('/', function(req, res, next) {
-//     console.log(req.query)
-//     if (!req.query.strategy_id) res.end('ERR:PARAMETERS_VALIDATION_FAILED')
-//     var seneca = req.app.get('settings').seneca;
-//     seneca.act('role:signal_log,cmd:all', req.query, function(err, val) {
-//         //console.log('val-->',val)
-//         if (err) res.end(err)
-//         //val.data = sort_watchlist(val.data)
-//         val.strategy_id = req.query.strategy_id
-//         val.data.forEach(function(item) {
-//             item.trans_log = item.log.map((x) => {
-//                 return {
-//                     val0: x[0],
-//                     val1: x[1],
-//                     val2: x[2]
-//                 }
-//             })
-//         })
-//         //console.log(val.data[0].trans_log)
-//         res.render('signal_log', val);
-//     })
-// });
+
 routes.get('/one?', function(req, res, next) {
-    console.log(req.query)
+    // console.log(req.query)
     if (!req.query.tradingsymbol) res.end('ERR:PARAMETERS_VALIDATION_FAILED')
     var seneca = req.app.get('settings').seneca;
     seneca.make$('eod').list$(req.query, function(err, val) {
@@ -38,4 +16,20 @@ routes.get('/one?', function(req, res, next) {
         res.json(val);
     })
 });
+routes.get('/all?', function(req, res, next) {
+    // console.log(req.query)
+    var seneca = req.app.get('settings').seneca;
+    seneca.make$('eod').list$(function(err, val) {
+        //console.log('val-->',val)
+        if (err) res.end(err)
+        //val.data = sort_watchlist(val.data)
+     var final_val = {}
+        val.forEach(function(item){ final_val[item.tradingsymbol]=item.data$(false)})
+       
+        final_val.success = true
+        final_val.data = val
+        res.json(final_val);
+    })
+});
+
 module.exports.routes = routes;
